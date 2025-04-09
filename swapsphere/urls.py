cafeeb425ecaa3ca31e2d03bb6f7bd85684f2770
AndreyLapko1 <andreycgo765@gmail.com>
2025-04-10 00:45:08 +0300

    add register and login forms & views / create models

--- swapsphere/urls.py
+++ swapsphere/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from ads.views import Login, Register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', Login.as_view(), name='login'),
+    path('register/', Register.as_view(), name='register'),
 ]
