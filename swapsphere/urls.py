b4b3ea52f8aebaed8d33ecc69f4a7ed916fef57c
AndreyLapko1 <andreycgo765@gmail.com>
2025-04-12 20:30:34 +0300

    add redoc

--- swapsphere/urls.py
+++ swapsphere/urls.py
@@ -41,6 +41,7 @@ urlpatterns = [
     path('', RedirectView.as_view(url='/login/')),
     path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path('login/', Login.as_view(), name='login'),
     path('register/', Register.as_view(), name='register'),
