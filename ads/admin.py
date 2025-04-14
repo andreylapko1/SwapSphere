853808aa397f4356aab6f4e878b5067f7750ca3c
AndreyLapko1 <andreycgo765@gmail.com>
2025-04-14 21:49:11 +0300

    remake doc

--- ads/admin.py
+++ ads/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from ads.models import ExchangeProposal, Ads
+
+
+@admin.register(Ads)
+class AdsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ExchangeProposal)
+class ExchangeProposalAdmin(admin.ModelAdmin):
+    pass
+
 # Register your models here.
